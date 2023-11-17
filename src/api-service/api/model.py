15d442308ac340f735795f0d258045bf8d13291e
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-11-17 00:32:46 -0500

    docker in progress

--- src/api-service/api/model.py
+++ src/api-service/api/model.py
@@ -45,17 +45,6 @@ def load_prediction_model():
         data_details = json.load(json_file)
 
 
-def check_model_change():
-    global best_model, best_model_id
-    best_model_json = os.path.join(local_experiments_path, "best_model.json")
-    if os.path.exists(best_model_json):
-        with open(best_model_json) as json_file:
-            best_model = json.load(json_file)
-
-        if best_model_id != best_model["experiment"]:
-            load_prediction_model()
-            best_model_id = best_model["experiment"]
-
 
 def load_preprocess_image_from_path(image_path):
     print("Image", image_path)
@@ -108,43 +97,6 @@ def make_prediction(image_path):
         "prediction_shape": prediction.shape,
         "prediction_label": prediction_label,
         "prediction": prediction.tolist(),
-        "accuracy": round(np.max(prediction) * 100, 2),
-        "poisonous": poisonous,
-    }
-
-
-def make_prediction_vertexai(image_path):
-    print("Predict using Vertex AI endpoint")
-
-    # Get the endpoint
-    # Endpoint format: endpoint_name="projects/{PROJECT_NUMBER}/locations/us-central1/endpoints/{ENDPOINT_ID}"
-    endpoint = aiplatform.Endpoint(
-        "projects/129349313346/locations/us-central1/endpoints/8600804363952193536"
-    )
-
-    with open(image_path, "rb") as f:
-        data = f.read()
-    b64str = base64.b64encode(data).decode("utf-8")
-    # The format of each instance should conform to the deployed model's prediction input schema.
-    instances = [{"bytes_inputs": {"b64": b64str}}]
-
-    result = endpoint.predict(instances=instances)
-
-    print("Result:", result)
-    prediction = result.predictions[0]
-    print(prediction, prediction.index(max(prediction)))
-
-    index2label = {0: "oyster", 1: "crimini", 2: "amanita"}
-
-    prediction_label = index2label[prediction.index(max(prediction))]
-
-    poisonous = False
-    if prediction_label == "amanita":
-        poisonous = True
-
-    return {
-        "prediction_label": prediction_label,
-        "prediction": prediction,
         "accuracy": round(np.max(prediction) * 100, 2),
         "poisonous": poisonous,
     }
--- src/api-service/api/model_backend.py
+++ src/api-service/api/model_backend.py
@@ -1,16 +1,26 @@
 import io
-
 from flask import Flask, request, jsonify
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import AutoTokenizer, BitsAndBytesConfig
+from llava.model import LlavaLlamaForCausalLM
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
 from flasgger import Swagger
 from PIL import Image
 from flask import Flask, jsonify
 from flasgger import Swagger, SwaggerView, Schema, fields
-
+import torch
 
 # Load pre-trained model and tokenizer
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+        "cnut1648/llava-v1.5-7b", 
+        model_name="llava-v1.5-7b", 
+        model_base=None, 
+        load_8bit=False, 
+        load_4bit=True
+)
 
 class Prompt(Schema):
     prompt = fields.Str(required=True)
@@ -31,7 +41,7 @@ class ChatView(SwaggerView):
     def post(self):
         """Chat with our model!"""
         # Extract text data from form-data
-        prompt = request.form.get("prompt")
+        cur_prompt = request.form.get("prompt")
         temperature = float(request.form.get("temperature", 1.0))
         print(request.form)
         history = request.form.getlist("history")
@@ -40,21 +50,87 @@ class ChatView(SwaggerView):
         # Check if an image is provided and process it
         if 'image' in request.files:
             image_file = request.files['image']
-            image = Image.open(io.BytesIO(image_file.read()))
+            image = Image.open(io.BytesIO(image_file.read())).convert('RGB')
             print(image)
+            image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            images = image_tensor.unsqueeze(0).half().cuda()
+            if getattr(model.config, 'mm_use_im_start_end', False):
+                qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+            cur_prompt = '<image>' + '\n' + cur_prompt
+        else:
+            images = None
 
-        # Tokenize the prompt
-        inputs = tokenizer.encode(prompt, return_tensors='pt')
+        conv_mode = "llava_v0"
+        conv = conv_templates[conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
 
-        # Generate a response from the model
-        outputs = model.generate(inputs, max_length=100, temperature=temperature)
-        response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        # remove prompt from response
-        response_text = response_text.replace(prompt, "")
+        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        keywords = [stop_str]
+        stopping_criteria = [KeywordsStoppingCriteria(keywords, tokenizer, input_ids)] if conv.version == "v0" else None
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images,
+                do_sample=True,
+                temperature=temperature,
+                max_new_tokens=1024,
+                use_cache=True,
+                stopping_criteria=stopping_criteria,
+            )
+
+        input_token_len = input_ids.shape[1]
+        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        if n_diff_input_output > 0:
+            print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
+        outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        outputs = outputs.strip()
+        if outputs.endswith(stop_str):
+            outputs = outputs[:-len(stop_str)]
+        outputs = outputs.strip()
+
+        # prompt for answer
+        outputs_reasoning = outputs
+        input_ids = tokenizer_image_token(prompt + outputs_reasoning + ' ###\nANSWER:', tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images,
+                do_sample=True,
+                temperature=temperature,
+                max_new_tokens=64,
+                use_cache=True,
+                stopping_criteria=[stopping_criteria])
+
+        input_token_len = input_ids.shape[1]
+        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        if n_diff_input_output > 0:
+            print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
+        outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        outputs = outputs.strip()
+        if outputs.endswith(stop_str):
+            outputs = outputs[:-len(stop_str)]
+        outputs = outputs.strip()
+        outputs = outputs_reasoning + '\n The answer is ' + outputs
+
+        # # Tokenize the prompt
+        # inputs = tokenizer.encode(prompt, return_tensors='pt')
+
+        # # Generate a response from the model
+        # outputs = model.generate(inputs, max_length=100, temperature=temperature)
+        # response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        # # remove prompt from response
+        # response_text = response_text.replace(prompt, "")
 
         # Return the response
-        return jsonify({"response": response_text})
-
+        return jsonify({"response": outputs})
 
 # Initialize the Flask application
 app = Flask(__name__)
