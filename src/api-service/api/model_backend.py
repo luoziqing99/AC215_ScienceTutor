a0ed7d123b5f697d35f82be6a38b7b790c9849be
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-11-17 17:49:22 -0500

    in progress

--- src/api-service/api/model_backend.py
+++ src/api-service/api/model_backend.py
@@ -1,36 +1,18 @@
-import io
 from flask import Flask, request, jsonify
-
+from flasgger import Swagger, SwaggerView, Schema, fields
+from io import BytesIO
+import torch
+import requests
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
 from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
-from flasgger import Swagger
-from PIL import Image
-from flask import Flask, jsonify
-from flasgger import Swagger, SwaggerView, Schema, fields
-import torch
-import requests
-
-import torch
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
-from llava.conversation import conv_templates, SeparatorStyle
-from llava.model.builder import load_pretrained_model
-from llava.utils import disable_torch_init
-from llava.mm_utils import process_images, tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
-
-from PIL import Image
-
-import requests
-from PIL import Image
-from io import BytesIO
 from transformers import TextStreamer
-
+from PIL import Image
 
 # Load pre-trained model and tokenizer
 disable_torch_init()
-
 model_path = "liuhaotian/llava-v1.5-7b"
 model_name = get_model_name_from_path(model_path)
 tokenizer, model, image_processor, context_len = load_pretrained_model(
@@ -61,20 +43,19 @@ class ChatView(SwaggerView):
         """Chat with our model!"""
         # Extract text data from form-data
         qs = request.form.get("prompt")
-        cur_prompt = qs
         temperature = float(request.form.get("temperature", 1.0))
         print(request.form)
         history = request.form.getlist("history")
         print("Provided history: ", history)
 
         # Check if an image is provided and process it
+        cur_prompt = qs
         if 'image' in request.files:
             image_file = request.files['image']
             response = requests.get(image_file)
             image = Image.open(BytesIO(response.content)).convert('RGB')
-            image_tensor = process_images([image], image_processor, model.config)
-            image_tensor = image_tensor.to(model.device, dtype=torch.float16)
-
+            image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            images = image_tensor.unsqueeze(0).half().cuda()
             if getattr(model.config, 'mm_use_im_start_end', False):
                 qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
             else:
@@ -89,10 +70,12 @@ class ChatView(SwaggerView):
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
 
-        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
+        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
         stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
         keywords = [stop_str]
-        stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+        stopping_criteria = [KeywordsStoppingCriteria(keywords, tokenizer, input_ids)] if conv.version == "v0" else None
+
         streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
         with torch.inference_mode():
@@ -104,9 +87,18 @@ class ChatView(SwaggerView):
                 max_new_tokens=1024,
                 streamer=streamer,
                 use_cache=True,
-                stopping_criteria=[stopping_criteria])
+                stopping_criteria=stopping_criteria,
+            )
 
-        outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+        input_token_len = input_ids.shape[1]
+        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        if n_diff_input_output > 0:
+            print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
+        outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        outputs = outputs.strip()
+        if outputs.endswith(stop_str):
+            outputs = outputs[:-len(stop_str)]
+        outputs = outputs.strip()
         conv.messages[-1][-1] = outputs
         
         # Return the response
