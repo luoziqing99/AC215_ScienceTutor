27dc7f8993a22e55611184078ae8c9bfffa20a1f
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-12-11 18:36:38 -0500

    /run-deploy-app

--- src/app_deploy/deploy-app.sh
+++ src/app_deploy/deploy-app.sh
@@ -1,4 +1,4 @@
-# # Build and Push Docker Containers to GCR
+# Build and Push Docker Containers to GCR
 ansible-playbook deploy-docker-images.yml -i inventory.yml
 # Update Cluster
 ansible-playbook update-k8s-cluster.yml -i inventory-prod.yml
